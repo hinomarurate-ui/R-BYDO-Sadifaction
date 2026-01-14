10ce9aded899175775b110bdb6372894e2922ebe
S4Tia2 <hinomarurate@gmail.com>
2026-01-14 18:27:39 +0900

    1/14
    ジャンプ軌道の修正

--- Assets/Enemy/Enemy Script Folder/EnemyMove.cs
+++ Assets/Enemy/Enemy Script Folder/EnemyMove.cs
@@ -12,7 +12,17 @@ public class EnemyMove : MonoBehaviour
     [SerializeField]
     Enemy enemy;
     [SerializeField]
-    float JumpV;
+    float JumpImpulse;
+    [SerializeField]
+    float JumpPower;
+    [SerializeField]
+    float JumpSlow;
+    [SerializeField]
+    float MaxHeight;
+
+    float JumpstartY;
+
+    bool Jumping;
 
     private Animator anim = null;
     
@@ -24,13 +34,13 @@ public class EnemyMove : MonoBehaviour
 
     void Start()
     {
+        Jumping = false;
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         enemy = GetComponent<Enemy>();
     }
     public void MoveX(float Dirx)
     {
-        Debug.Log("ばなな");
         anim.SetBool("Jump", false);
         rb.velocity = new Vector2(Dirx * MoveSpeed, rb.velocity.y);
     }
@@ -40,8 +50,10 @@ public class EnemyMove : MonoBehaviour
     {
         if(enemy.isGround)
         {
+            Jumping = false;
             if(enemy.isGake)
             {
+                if(!Jumping)
                 StartCoroutine(JumpCharge());
                 
             }
@@ -50,20 +62,55 @@ public class EnemyMove : MonoBehaviour
                 MoveX(Dirx);
             }
         }
+
+        
         
     }
 
+    void FixedUpdate()
+    {
+        float startY = JumpstartY + JumpSlow;
+        float endY = JumpstartY + MaxHeight;
+
+        if(Jumping != true)
+        {
+            return;
+        }
+
+        if(rb.velocity.y >= 0f)
+        {
+            if(rb.position.y < startY)
+            {
+                rb.AddForce(Vector2.up * JumpPower,ForceMode2D.Force);
+            }
+            else
+            {
+                float T = Mathf.InverseLerp(startY,endY,rb.position.y);
+                float thrust = Mathf.Lerp(JumpPower,0,T);
+                rb.AddForce(Vector2.up * thrust, ForceMode2D.Force);
+            }
+
+        }
+    }
     void Jump(float Dirx)
     {
+        Jumping = true;
         anim.SetBool("Jump", true);
         anim.SetBool("JumpC", false);
-        rb.velocity = new Vector2(Dirx * MoveSpeed, JumpV);
+        rb.AddForce(Vector2.up * JumpImpulse,ForceMode2D.Impulse);
+        rb.velocity = new Vector2(Dirx * MoveSpeed, rb.velocity.y);
     }
     IEnumerator JumpCharge()
     {
+        JumpstartY = rb.position.y;
         anim.SetBool("JumpC", true);
         yield return new WaitForSeconds(0.5f);
         Jump(Dirx);
          yield return new WaitForSeconds(0.3f);
+    }
+
+    void Fall()
+    {
+        rb.velocity = new Vector2(Dirx * MoveSpeed, rb.velocity.y - 0.3f);
     }
 }
